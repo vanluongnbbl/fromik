92972963350317eebb61deda2996e10c03c7e5a4
luong <vanluongnbbl@gmail.com>
2022-04-09 15:54:05 +0700

    first component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import YoutubeForm from './components/YoutubeForm';
+
+function App() {
+  return (
+    <div className="App">
+      <YoutubeForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/YoutubeForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function YoutubeForm() {
+  return (
+    <div>YoutubeForm</div>
+  )
+}
+
+export default YoutubeForm
